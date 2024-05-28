5989a68e14ad97e31a0ea30ac614e30074bcd76c
Dick Hollenbeck <dick@softplc.com>
2024-05-28 14:43:08 -0500

    build*.sh: bump tag and omit docker hub push

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,7 +5,7 @@
 # and can be used locally.  SoftPLC Corp has this image for the time being
 # at docker hub.
 
-VERSION_TAG=":1.0"
+VERSION_TAG=":1.1"
 
 IMAGE="softplc/runtime"
 
@@ -13,4 +13,4 @@ NOCACHE='--no-cache --pull'
 
 docker build $NOCACHE -t $IMAGE$VERSION_TAG -t $IMAGE:latest .
 
-docker push -a $IMAGE
+#docker push -a $IMAGE
